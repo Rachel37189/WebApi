781e5d5bc556f64a1baa04f3ac70db623d1dba56
efratleibovitz <e0548534615@gmail.com>
2025-11-09 19:31:47 +0200

    update in colaborator :) - Rachel project

--- WebApiShop/User.cs
+++ WebApiShop/User.cs
@@ -2,9 +2,21 @@
 {
     public class User
     {
+         [Required(ErrorMessage = "חובה להזין אימייל")]
+        [EmailAddress(ErrorMessage = "כתובת אימייל לא חוקית")]
         public string userName { get; set; } = string.Empty;
+        
+        
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "השם חייב להיות בין 3 ל-20 תווים")]
         public string fName { get; set; } = string.Empty;
+
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "השם חייב להיות בין 3 ל-20 תווים")]
         public string lName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "חובה להזין סיסמה")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "הסיסמה חייבת להיות באורך של 8 עד 20 תווים")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).+$", 
+        ErrorMessage = "הסיסמה חייבת להכיל לפחות אות גדולה, אות קטנה, מספר ותו מיוחד")]
         public int passWord { get; set; } 
         public int id { get; set; }
     }
